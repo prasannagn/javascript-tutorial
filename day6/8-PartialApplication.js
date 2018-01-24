13f24ecbed2e922d350391b332ada7d91147aff9
prasannagn <prasanna.nagabhushana@gmail.com>
2018-01-24 17:00:49 +0530

    Update 8-PartialApplication.js

--- day6/8-PartialApplication.js
+++ day6/8-PartialApplication.js
@@ -1,3 +1,29 @@
+//Add 3 numbers
+
+let add = (a,b,c)=>a+b+c;
+
+let curry = (add, first) => {
+    return (secend) => {
+        return (third) =>{
+            return add(first , secend , third);
+        }
+    }
+};
+
+console.log(curry(1)(2)(3));
+
+
+
+
+let partial = (add, first, second) =>{
+    return (third) =>{
+        return add(first , second , third);
+    };
+};
+
+console.log(partial(1)(2,3));
+
+
 //arity
 
 function test(a,b,c) {
